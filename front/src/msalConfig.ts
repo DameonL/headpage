a309a53874620ce96fdd25a8c214c4418bb53c00
Dameon Laird <dameonlair@gmail.com>
2023-10-13 16:48:07 -0700

    Updated config

--- front/src/msalConfig.ts
+++ front/src/msalConfig.ts
@@ -1,19 +1,20 @@
 export const msalConfig = {
   auth: {
-    clientId: "36148247-7edd-499a-b4d2-6f37380a4f57",
-    authority: "https://login.microsoftonline.com/b50567c1-417f-41a0-a60b-1800062c92b5", // This is a URL (e.g. https://login.microsoftonline.com/{your tenant ID})
+    clientId: "900f4f61-a401-44ce-990f-d5cb183dc9de",
+    authority: "https://login.microsoftonline.com/329b180e-d58e-4ff8-8774-bcb44323ec55", // This is a URL (e.g. https://login.microsoftonline.com/{your tenant ID})
     redirectUri: import.meta.env.DEV ? "http://localhost:4173" : "https://nice-flower-001d0cb1e.3.azurestaticapps.net",
+    navigateToLoginRequestUrl: false, // If "true", will navigate back to the original request location before processing the auth code response.
   },
   cache: {
     cacheLocation: "localStorage",
     storeAuthStateInCookie: false,
-  }
+  },
 };
 
 export const loginRequest = {
- scopes: ["User.Read", "offline_access", "api://c4a52562-6388-47ef-bf9e-ba63750510e4/identity"]
+  scopes: [],
 };
 
 export const graphConfig = {
-    graphMeEndpoint: "https://graph.microsoft.com/v1.0/me"
+  graphMeEndpoint: "https://graph.microsoft.com/v1.0/me",
 };
